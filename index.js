1136634a38f17a7f36dbf00616edaa7dfad44566
Victor Dias <victordias07@gmail.com>
2020-07-07 03:30:58 -0300

    Fixed clustering for good

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+/**
+ * Load .env
+ */
+require('dotenv').config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const leaveEvent = require('./events/leave.event')
+const cluster = require('cluster')
 
 const commands = {
 	// 'wm': require('./commands/welcome.command'),
@@ -10,48 +15,48 @@ const commands = {
  * Clustering.
  */
 if((process.env.ENV === 'PROD' || process.env.CLUSTERING === "TRUE") && cluster.isMaster) {
-    let workers = [];
+    let workers = []
     // to read number of cores on system
-    let numCores = require('os').cpus().length;
-    console.log('Master cluster setting up ' + numCores + ' workers');
+    let numCores = require('os').cpus().length
+    console.log('[PA] BOT ITS CREATING ' + numCores + ' CLUSTERS')
     
     // iterate on number of cores need to be utilized by an application
     // current example will utilize all of them
     for(let i = 0; i < numCores; i++) {
         // creating workers and pushing reference in an array
         // these references can be used to receive messages from workers
-        workers.push(cluster.fork());
+        workers.push(cluster.fork())
     
         // to receive messages from worker process
         workers[i].on('message', function(message) {
-            console.log(message);
-        });
+            console.log(message)
+        })
     }
     
     // process is clustered on a core and process id is assigned
     cluster.on('online', function(worker) {
-        console.log('Worker ' + worker.process.pid + ' is listening');
-    });
+        console.log('[PA] CLUSTER ' + worker.process.pid + ' IS LISTENING')
+    })
     
     // if any of the worker process dies then start a new one by simply forking another one
     cluster.on('exit', function(worker, code, signal) {
-        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-        console.log('Starting a new worker');
-        workers.push(cluster.fork());
+        console.log('[PA] CLUSTER ' + worker.process.pid + ' DIED WITH CODE : ' + code + ' AND SIGNAL: ' + signal)
+        console.log('[PA] STARTING NEW CLUSTER')
+        workers.push(cluster.fork())
         // to receive messages from worker process
         workers[workers.length-1].on('message', function(message) {
-            console.log(message);
-        });
-    });
+            console.log(message)
+        })
+    })
 } else { 
 	client.on("ready", () => {
-		console.log(`Logged in as ${client.user.tag}!`)
+		console.log(`[PA] BOT ONLINE WITH NAME: ${client.user.tag}!`)
 	})
 	
 	client.on("message", message => {
-		const content = message.content;
+		const content = message.content
 		if(content.charAt(0) === "!") {
-			const command = content.substr(1);
+			const command = content.substr(1)
 			if(typeof commands[command] === 'function') {
 				commands[command]({ message, client })
 			} 

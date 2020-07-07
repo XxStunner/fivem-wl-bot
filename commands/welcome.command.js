5caf87fad2b9d82f57cacc7b48b7adeedc40e0c1
Victor Dias <victordias07@gmail.com>
2020-07-07 04:09:04 -0300

    Fixed welcome message

--- commands/welcome.command.js
+++ commands/welcome.command.js
@@ -9,24 +9,11 @@ module.exports = ({ message }) => {
             Olá !
             Tudo bem ?
             
-            Primeiramente agradecemos o seu interesse em fazer parte da nossa cidade. Ficaremos muito felizes em ter você conosco.
-            
-            Para liberação do seu passaporte, pedimos que preencha algumas informações que serão solicitadas a seguir. 
-            
-            As perguntas relacionadas as regras da cidade são simples e objetivas, que nos ajudam a manter uma convivência saudável entre nossos moradores. Recomendamos que leia atentamente as regras do servidor antes mesmo de fazer a whitelist: http://brasilplayaction.com.br/index.php?/forum/34-rules-of-server/
-            
-            Qualquer dúvida estamos a disposição no Discord, onde será divulgado o resultado da sua solicitação de 
-            passaporte.
-            
-            Para pegar o ID, utilize nosso launcher: https://bit.ly/2Zv31go
-            
-            Tudo pronto, vamos começar ?
+            Para realizar whitelist em nosso servidor, digite: 
 
-            Digite: 
-            
             \`\`\`\!whitelist\`\`\`\
 
-            Boa sorte!
+            E aguarde as instruções do BOT.
         `)
     message.channel.send(embed)
 }
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client()
 const leaveEvent = require('./events/leave.event')
 
 const commands = {
-	// 'wm': require('./commands/welcome.command'),
+	'wm': require('./commands/welcome.command'),
 	'whitelist': require('./commands/whitelist.command')
 }
 client.on("ready", () => {
